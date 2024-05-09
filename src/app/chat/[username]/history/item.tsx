b28f59f597b5d07a68451e534f2e955f1e6ae51d
SeyedMohammadHadi Hoseini <seyedmohadihoseini@gmail.com>
2024-05-09 21:15:43 +0330

    change sub

--- src/app/chat/[username]/history/item.tsx
+++ src/app/chat/[username]/history/item.tsx
@@ -6,11 +6,11 @@ import { useEffect, useLayoutEffect, useState } from "react";
 export default function HistoryItem({ message, isMine }: { message: MessageType, isMine: boolean }) {
 
     let messageDate = DifferentOfTwoTime(message.createdDate, new Date());
-
+    const currentDate = message.createdDate;
     return <li className="message-li">
         <p className={isMine ? "bg-slate-500" : "float-end bg-orange-500"}>
             {message.text}
-            <sub>{messageDate}</sub>
+            <sub>{`       ${currentDate.getHours()}:${currentDate.getMinutes()}`}</sub>
         </p>
     </li>
 }
